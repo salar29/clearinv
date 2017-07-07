d5a42b2ec2c2e8d3148a4c221dfb7221749ece05
salar29 <salar38ics@gmail.com>
2017-07-07 22:51:54 +0200

    Add files via upload

--- server/clear.lua
+++ server/clear.lua
@@ -1,13 +1,13 @@
-function OnPlayerChat(args)
-	local inventory = args.player:GetInventory()
-		if args.text == "/clear" then
-			if table.count(inventory) == 0 then
-				Chat:Send(args.player, "You aren't carrying any weapons", Color.Red)
-			else
-				args.player:ClearInventory()
-				Chat:Send(args.player, "Your inventory has been cleared", Color.Lime)
-			end	
+function Clear(args)
+	if string.lower(args.text) == "/clear" then
+		if table.count(args.player:GetInventory()) == 0 then
+			Chat:Send(args.player, "You aren't carrying any weapons", Color.Red)
+		else
+			args.player:ClearInventory()
+			Chat:Send(args.player, "Your inventory has been cleared", Color.Lime)
 		end
+		return false
+	end
 end
 
-Events:Subscribe("PlayerChat", OnPlayerChat)
+Events:Subscribe("PlayerChat", Clear)
